18b9181ac4a1d029da9341a69b7b58d19dce7179
Aragya1642 <agoyal1642@gmail.com>
2024-02-03 01:42:54 -0500

    Working Recorder

--- SoundGui.py
+++ SoundGui.py
@@ -1,2 +1,14 @@
-import time
+import sounddevice as sd
+from scipy.io.wavfile import write
 
+fs = 44100 # Sampling Rate
+duration = 5 # Duration of recording
+
+# Set the defaults for sounddevice module
+sd.default.samplerate = fs
+sd.default.channels = 2
+
+# Perform recording and save as a wav file
+myRecording = sd.rec(int(duration * fs))
+sd.wait()
+write('output.wav', fs, myRecording)
